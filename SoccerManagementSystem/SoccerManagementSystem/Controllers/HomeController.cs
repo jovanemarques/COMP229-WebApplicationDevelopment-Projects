06f0267af44e686299921697d79bd005b93d28a2
Jovane Marques <jovanemarques@gmail.com>
2019-01-30 01:35:48 -0500

    SoccerManagementSystem

---
+++ SoccerManagementSystem/SoccerManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SoccerManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();                                                                                                                                                                                                                                          
+        }
+        public ViewResult Club()
+        {
+            return View();
+        }
+        public ViewResult AddClub()
+        {
+            return View();
+        }
+        public ViewResult ShowClubDetails()
+        {
+            return View();
+        }
+        public ViewResult ManagePlayers()
+        {
+            return View();
+        }
+    }
+}
