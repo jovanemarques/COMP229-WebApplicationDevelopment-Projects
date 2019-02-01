2adf67821e7217f3399d7549a1d63643d85d9ecf
Jovane Marques <jovanemarques@gmail.com>
2019-02-01 14:31:02 -0500

    Class Feb, 01 - razor   - imports with file _ViewImports.cshtml - page layout   - view start file - conditional expressions - foreach - NuGet - Manage Client-Side Libraries

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Product myProduct = new Product
+            Product[] productArray = new[]{
+                new Product
+                {
+                    ProductID = 1,
+                    Name = "Kayak",
+                    Description = "A boat",
+                    Price = 275M,
+                    Category = "Watersports"
+                },
+                  new Product
+                  {
+                      Name = "Lifejacket",
+                      Price = 99M
+                  },
+                new Product
+                {
+                    Name = "Ball",
+                    Price = 29.99M
+                },
+                new Product
+                {
+                    Name = "Lifejacket",
+                    Price = 99M
+                } }
+                ;
+
+            //ViewBag.StockLevel = 4;
+            //ViewBag.StockLevel = 0;
+            ViewBag.StockLevel = 2;
+
+            //return View(myProduct);
+            return View(productArray);
+        }
+    }
+}
---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio/Models/SimpleRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace WorkingWithVisualStudio.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+        public static SimpleRepository SharedRepository => sharedRepository;
+        public SimpleRepository()
+        {
+            var initialItems = new[]
+            {
+                new Product{ Name="Kayak", Price=275M},
+                new Product{ Name="Lifejacket", Price=49.99M},
+                new Product{ Name="Ball", Price=29.99M},
+                new Product{ Name="Flag", Price=9.99M}
+            };
+            foreach (var p in initialItems)
+            {
+                AddProducts(p);
+            }
+        }
+
+        public void AddProducts(Product p) => products.Add(p.Name, p);
+        public IEnumerable<Product> Produtcs => products.Values;
+    }
+}
