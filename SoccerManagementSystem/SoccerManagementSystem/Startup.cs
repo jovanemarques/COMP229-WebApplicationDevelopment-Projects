051788a10a4b654d7e45a6d60645170817a91221
Jovane Marques <jovanemarques@gmail.com>
2019-02-06 00:52:26 -0500

    Assignment 1 - Structure

--- SoccerManagementSystem/SoccerManagementSystem/Startup.cs
+++ SoccerManagementSystem/SoccerManagementSystem/Startup.cs
@@ -21,6 +21,7 @@ namespace SoccerManagementSystem
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
