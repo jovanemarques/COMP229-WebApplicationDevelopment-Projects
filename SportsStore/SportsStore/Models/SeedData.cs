cf4802cff2916ad425828755f6250b689f053cd8
Jovane Marques <jovanemarques@gmail.com>
2019-02-08 14:56:18 -0500

    class on 2019-01-08

---
+++ SportsStore/SportsStore/Models/SeedData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product { Name = "Kayay", Description = "A boat for one person", Category = "Watersports", Price = 257 }
+                    );
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- SportsStore/SportsStore/Program.cs
+++ SportsStore/SportsStore/Program.cs
@@ -19,6 +19,6 @@ namespace SportsStore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseDefaultServiceProvider(option => option.ValidateScopes=false);
     }
 }
