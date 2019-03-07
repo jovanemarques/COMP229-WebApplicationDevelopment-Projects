d9427a3428c40978c65f0c0af55f2e5db4a5a880
Jovane Marques <jovanemarques@gmail.com>
2019-03-07 12:31:19 -0500

    new SportStore with changes from the missed class in 2019-02-22

---
+++ SportStore_Tatiana/SportStore/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportStore.Infrastructure;
+using SportStore.Models;
+using SportStore.Models.ViewModels;
+
+namespace SportStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+            .FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                Cart cart = GetCart(); // I get the cart from the session
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+        return RedirectToAction("Index", new { returnUrl });
+        }
+        public RedirectToActionResult RemoveFromCart(int productId,
+        string returnUrl)
+        {
+            Product product = repository.Products
+            .FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return cart;
+        }
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+        }
+    }
+}
---
+++ SportStore_Tatiana/SportStore/Models/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportStore.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; } // collection of products - it will go to the DB
+
+    }
+}
