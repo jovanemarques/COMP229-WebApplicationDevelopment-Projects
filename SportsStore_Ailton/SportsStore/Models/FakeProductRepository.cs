ac717bb1f7fe7d3e43606493b56d581353b1d314
Jovane Marques <jovanemarques@gmail.com>
2019-03-07 12:32:24 -0500

    new SportStore with changes from the missed class in 2019-02-22

---
+++ SportsStore_Ailton/SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>()
+        {
+            new Product() { Name = "Football", Price = 25M },
+            new Product() { Name = "Flag", Price = 20M },
+            new Product() { Name = "Running shoes", Price = 70M }
+        }.AsQueryable<Product>();
+    }
+}
---
+++ SportsStore_Ailton/SportsStore/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get;  }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(opts => opts.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: null, template: "{category}/Page{productPage:int}", 
+                    defaults: new { Controller = "Product", action = "List" });
+                routes.MapRoute(name: "pagination", template: "Page{productPage:int}", 
+                    defaults: new { Controller = "Product", action = "List", productPage=1 });
+                routes.MapRoute(name: "category", template: "{category}", 
+                    defaults: new { Controller = "Product", action = "List", productPage=1 });
+                routes.MapRoute(name: null, template: "", 
+                    defaults: new { Controller = "Product", action = "List", productPage=1 });
+
+                routes.MapRoute(name: "default", template: "{controller=Product}/{action=List}/{id?}");
+            });
+            SeedData.EnsurePopulated(app);
+        }
+    }
+}
