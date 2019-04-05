2033162bf6aaf28655f2f339b6993253c6b2c7b9
Jovane Marques <jovanemarques@gmail.com>
2019-04-05 08:55:39 -0400

    lecture 2019-04-01

---
+++ ControllersAndActions/ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Result", "This is derived controller");
+        }
+        public ViewResult Headers() => View("DictionaryResult", Request.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First()));
+    }
+}
---
+++ ControllersAndActions/ControllersAndActions/Controllers/PocoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllersAndActions.Controllers
+{
+    public class PocoController//:Controller
+    {
+        [ControllerContext]
+        public ControllerContext ControllerContext { get; set; }
+        //public string Index() => "This is POCO controller";
+
+        //public ViewResult Index()
+        //{
+        //    return new ViewResult()
+        //    {
+        //        ViewName = "Result",
+        //        ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(),
+        //                   new ModelStateDictionary())
+        //        {
+        //            Model = "This is a POCO controller"
+        //        }
+        //    };
+        //}
+
+        //public ViewResult Index() => View("Result", "This is my POCO controller 3");
+
+        public ViewResult Headers()
+        {
+            return new ViewResult()
+            {
+                ViewName = "DictionaryResult",
+                ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(),
+                           new ModelStateDictionary())
+                {
+                    Model = ControllerContext.HttpContext.Request.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First())
+                }
+            };
+        }
+    }
+}
